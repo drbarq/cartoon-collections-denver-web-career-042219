687e8181153be622c1086ad5039684992d06b16a
Joe Tustin <j.tustin@gmail.com>
2019-03-15 15:58:51 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
 def roll_call_dwarves(dwarves)
-  dwarves.each_with_index {|name, index| puts index ; puts name}
+  dwarves.each_with_index {|name, index| puts "#{index}.#{name}"}
 
   # Your code here
 end
