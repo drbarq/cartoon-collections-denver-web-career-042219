3ca99de84912e3bd83506d3fa9a670497639bde3
Joe Tustin <j.tustin@gmail.com>
2019-03-15 17:05:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,6 +15,9 @@ def find_the_cheese(foods)
   foods.find{|food| cheese_types.include?(food)}
 end
 
+## took a while to figure out I couldnt nest the includes but i guess it is becuase of the return vlaue
+
+
 ##foods = ["banana", "cheddar", "sock"]
 
 ##elements = ["carrot", "cucumber", "pepper"]
