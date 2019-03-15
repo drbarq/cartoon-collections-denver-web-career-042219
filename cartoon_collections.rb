60f2228de1f640cde5a8965fd68a5df1c19a9b90
Joe Tustin <j.tustin@gmail.com>
2019-03-15 16:48:30 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,10 +11,9 @@ def long_planeteer_calls(words)
 end
 
 def find_the_cheese(foods)
-  # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 
-  foods.include?{|food| cheese_types.each{|cheese|}
+  foods.include?{|food| "cheese_types"}
 
 end
 
