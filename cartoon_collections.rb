0806da73465c6e59977ea2cc99dca1e2c3f5b8d8
Joe Tustin <j.tustin@gmail.com>
2019-03-15 17:04:48 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,6 @@ end
 def find_the_cheese(foods)
   cheese_types = ["cheddar", "gouda", "camembert"]
   foods.find{|food| cheese_types.include?(food)}
-
 end
 
 ##foods = ["banana", "cheddar", "sock"]
