f89a7eb5228041513654684fd54d51e8c5541198
Joe Tustin <j.tustin@gmail.com>
2019-03-15 17:05:34 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,25 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index {|name, index| puts "/#{index+1}.*#{name}/"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(elements)
+  elements.collect {|element| "#{element.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(words)
+  words.any? {|word| word.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(foods)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  foods.find{|food| cheese_types.include?(food)}
 end
+
+## took a while to figure out I couldnt nest the includes but i guess it is becuase of the return value
+
+
+##foods = ["banana", "cheddar", "sock"]
+
+##elements = ["carrot", "cucumber", "pepper"]
+
+##words = ["axe", "earth", "wind", "fire"]
